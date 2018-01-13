336615acca6633385726bcb933b4d9ba3cdb8bf9
Bogdan Grigore <bogdan9310@gmail.com>
2018-01-13 13:26:34 +0200

    update pagina login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -16,9 +16,41 @@ class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        $user = $request->get('user');
-        $password = $request->get('password');
-        $ipAdress = $request->getClientIp();
-        return $this->render('login.html.twig', []);
+        $error = '';
+        $submitAction = $request->get('submitAction');
+        if (!empty($submitAction)) {
+            $email = trim($request->get('email'));
+            $password = $request->get('password');
+            $ipAddress = $request->getClientIp();
+            //verificat ca email si pass nu sunt empty. empty($var)
+
+            if (empty($email)) {
+                $error .= 'Va rog introduceti adresa de e-mail';
+            }
+            if (empty($password)) {
+                $error .= 'Va rog introduceti parola';
+            }
+            if (empty($error)) {
+                //api
+                $response = '{"isError":false,"errorMessage":[],"userInformation":{"userId":1,"firstName":"Alexandru","lastName":"Mihai","role":"SUPERUSER"}}';
+                $response = json_decode($response);
+                if ($response['isError'] === false) {
+                    setcookie('thorocea',$response['userId'].'_'.$ipAddress);
+                    $this->redirectToRoute('homepage');
+                }
+                $error .= $response['errorMessage'];
+
+
+            }
+        }
+
+        return $this->render(
+            'login.html.twig',
+            [
+                'errorMessage' => $error,
+                'lastEmail' => $email ?? ''
+            ]
+        );
+
     }
 }
