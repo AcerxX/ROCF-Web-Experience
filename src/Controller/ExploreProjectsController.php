2d07a6f0d857fb6da8306eb02955291d071ac6a7
Alexandru Mihai <alexandru.mihai@bitrix24.com>
2018-04-29 18:54:59 +0300

    implement search

--- src/Controller/ExploreProjectsController.php
+++ src/Controller/ExploreProjectsController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 
 use App\Security\WebserviceUser;
 use App\Service\ApiService;
-use function print_r;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ExploreProjectsController extends Controller
 {
@@ -34,6 +34,41 @@ class ExploreProjectsController extends Controller
             [
                 'user' => $user,
                 'projects' => $projects['data'],
+                'categories' => $categories
+            ]
+        );
+    }
+
+
+    public function search(Request $request, ApiService $apiService)
+    {
+        $parameterBag = [];
+
+        $searchedText = $request->get('searched_text');
+        if (null !== $searchedText) {
+            $parameterBag['searched_text'] = $searchedText;
+        }
+
+        $categoryId = $request->get('category_id');
+        if (null !== $categoryId) {
+            $parameterBag['category_id'] = $categoryId;
+        }
+
+        $projects = $apiService->callProjectsEngineApi(ApiService::ROUTE_PE_GET_PROJECTS_LISTING, $parameterBag);
+
+        $categories = $apiService->callProjectsEngineApi(
+            ApiService::ROUTE_PE_GET_CATEGORIES,
+            [],
+            ApiService::METHOD_GET
+        );
+
+        return $this->render(
+            'Explore/searchResults.html.twig',
+            [
+                'user' => $this->getUser(),
+                'projects' => $projects['data'],
+                'searchedText' => $searchedText,
+                'searchedCategoryId' => $categoryId,
                 'categories' => $categories
             ]
         );
